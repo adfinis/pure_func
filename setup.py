1ac5e9a15feb92731d5e1ee02878d1cc8fd0d6ce
Lucas Wiman <lucas.wiman@gmail.com>
2017-04-18 22:24:24 -0700

    Fix homepage link
    The one currently on pypi 404s: https://pypi.python.org/pypi/pure-func/1.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     description = "Pure-func helps to write pure functions in python",
     long_description = README_TEXT,
     keywords = "pure functional programming unit-testing",
-    url = "http://github.com/adfinis-sygroup/pure-func",
+    url = "http://github.com/adfinis-sygroup/pure_func",
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
